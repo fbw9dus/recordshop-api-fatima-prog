bb2781f58b2bb08d0c2d79c546559af39f370f71
fatima <benyamna2011@outlook.de>
2020-04-28 16:08:37 +0200

    letste commit

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,25 +1,29 @@
 const User = require('../models/User')
-const {validationResult} = require('express-validator')
+const { validationResult } = require('express-validator')
 const createError = require("http-errors")
 const encryption = require('../lib/validation/encryption')
 
 exports.getUsers = async (req, res, next) => {
   // Schreib hier code um alle Kunden aus der users-Collection zu holen
   const users = await User.find()
+  .select("-password")
+  .sort("lastName")
+  .limit(5)
+  
   res.status(200).send(users);
 };
 
 exports.getUser = async (req, res, next) => {
   const { id } = req.params;
   // Schreib hier code um den Kunden mit der id aus params aus der users-Collection zu holen
-  const user = await User.findById(id)
+  const user = await User.findById(id).select("-password")
   res.status(200).send(user);
 };
 
 exports.deleteUser = (req, res, next) => {
   const { id } = req.params;
   // Schreib hier code um den Kunden mit der id aus params aus der users-Collection zu löschen
-
+const user =  User.findByIdAndDelete(id).select("-password")
   res.status(200).send(user);
 };
 
@@ -28,7 +32,7 @@ exports.updateUser = async (req, res, next) => {
     const user = await User.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true
-    });
+    }).select("-password");
     if (!user) throw new createError.NotFound();
     res.status(200).send(user);
   } catch (e) {
@@ -53,29 +57,29 @@ exports.addUser = async (req, res, next) => {
 
     res
 
-    .status(200)
-    .header("x-auth", token)
-    .send(newUser);
-    
+      .status(200)
+      .header("x-auth", token)
+      .send(newUser);
+
   } catch (error) {
     next(error)
   }
 
-  
+
 };
 
 exports.loginUser = async (req, res, next) => {
   const { email, password } = req.body
 
   try {
-    const user = await User.findOne({ email }).select('+password')
+    let user = await User.findOne({ email }).select("+password")
     const valid = encryption.compare(password, user.password)
 
     const token = user.generateAuthToken()
     await user.save()
-    
-    if(!valid) throw new createError.NotFound()
 
+    if (!valid) throw new createError.NotFound()
+    user = await User.findOne({ email }).select("-password")
     res
       .status(200)
       .header("x-auth", token)
@@ -84,5 +88,5 @@ exports.loginUser = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
-  
+
 };
